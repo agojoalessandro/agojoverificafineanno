6f148fe2808357eae1b6229687a68e84f67e7b05
EC2 Default User <ec2-user@ip-172-31-84-40.ec2.internal>
2019-05-22 12:25:01 +0000

    punto 6

--- agojo-driving/src/app/app.module.ts
+++ agojo-driving/src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
 import { AppComponent } from './app.component';
 import { ReactiveFormsModule, FormsModule } from '@angular/forms';
 import { MezziautomaticiComponent } from './mezziautomatici/mezziautomatici.component';
@@ -11,7 +11,7 @@ import { MezziautomaticiComponent } from './mezziautomatici/mezziautomatici.comp
     MezziautomaticiComponent
   ],
   imports: [
-    BrowserModule, ReactiveFormsModule, FormsModule
+    BrowserModule, ReactiveFormsModule, FormsModule, NgbModule
   ],
   providers: [],
   bootstrap: [AppComponent]
